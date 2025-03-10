6bb71631f5a2b7f3987e69a9c5028efbf7bb9953
Philip Ecdao <phil@Philips-MacBook-Air.local>
2025-03-10 17:02:43 -0400

    updating missing code for submission

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,8 +5,21 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
-  await jwt.express.authWithPassword("pecdao@gmail.com", "123456");
-;
+export const authenticateToken = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    res.sendStatus(401);
+      return;
+    }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err: jwt.VerifyErrors | null, decoded: any) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      req.user = decoded as JwtPayload;
+      next();
+    }
+  });
 };
